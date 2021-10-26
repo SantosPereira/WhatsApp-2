48627d8263ff2e7537d3b9f2e3d0e9857c72f706
SantosPereira <pedrohenriquelemam@gmail.com>
2021-10-26 14:00:53 -0300

    Começo do desenvolvimento da interface

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator  } from '@react-navigation/native-stack'
+
+
+function Conversas() {
+  return (
+    <View>
+      <Text>Conversas</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function Status() {
+  return (
+    <View>
+      <Text>Status do zap</Text>
+    </View>
+  );
+}
+
+function Chamadas() {
+  return (
+    <View>
+      <Text>Chamadas</Text>
+    </View>
+  );
+}
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Conversas" component={Conversas}/>
+        <Stack.Screen name="Status" component={Status}/>
+        <Stack.Screen name="Chamadas" component={Chamadas}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
